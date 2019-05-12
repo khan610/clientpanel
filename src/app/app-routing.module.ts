4c15f6160e98a11856515f3533d5ba9f12c30a13
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-05-12 22:46:02 +0200

    Updated routing module ts
    Was missing a path for Component Details, added it and everything works fine now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'client/:id', component: ClientDetailsComponent},
   {path: 'client/add', component: AddClientComponent},
   {path: 'client/edit:id', component: EditClientComponent},
   {path: 'settings', component: SettingsComponent},
