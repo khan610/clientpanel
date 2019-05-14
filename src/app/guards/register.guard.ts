cb1f91fb3a4fa809c852c9ad01c03b8e8802c0ab
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-05-14 03:05:06 +0200

    Settings Service and Register Guard

---
+++ src/app/guards/register.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { SettingsService } from '../services/settings.service';
+
+@Injectable()
+export class RegisterGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private settingsService: SettingsService
+  ) {}
+
+  canActivate(): boolean{
+    //logic
+    if(this.settingsService.getSettings().allowRegistration) { 
+      return true;
+    } else {
+      this.router.navigate(['/login'])
+      return false;
+    }
+  }  
+}
